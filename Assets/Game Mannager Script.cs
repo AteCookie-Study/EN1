5df5fe97b22450187e68ddefd6265f9963664d8b
AteCookie <k024g1012@g.neec.ac.jp>
2025-04-24 11:52:00 +0900

    123

--- Assets/Game Mannager Script.cs
+++ Assets/Game Mannager Script.cs
@@ -5,16 +5,17 @@ public class GameMannagerScript : MonoBehaviour
 {
     public GameObject playerPrefab;
     int[,] map;
+    GameObject[,] objectsMap;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GameObject instance = Instantiate(\
-            playerPrefab,
-            new Vector3(0, 0, 0),
-            Quaternion.identity
-            );
+        //GameObject instance = Instantiate(
+        //    playerPrefab,
+        //    new Vector3(0, 0, 0),
+        //    Quaternion.identity
+        //    );
 
 
         map = new int[,] {
@@ -24,6 +25,26 @@ public class GameMannagerScript : MonoBehaviour
             { 0, 0, 0, 0, 0 },
 
         };
+        objectsMap = new GameObject[
+            map.GetLength(0),
+            map.GetLength(1)
+            ];
+
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                if (map[y, x] == 1)
+                {
+                    objectsMap[x, y] =
+                        Instantiate(
+                           playerPrefab,
+                           new Vector3(x, map.GetLength(0) - y, 0),
+                           Quaternion.identity
+                        );
+                }
+            }
+        }
         PrintArry();
     }
 
@@ -73,56 +94,53 @@ public class GameMannagerScript : MonoBehaviour
     }
 
 
-    //int GetPlayerIndex()
-    //{
-    //    // playerがいる位置の保存变数の宣言
-    //    int playerIndex = -1;
-    //    // playerの位置の検索
-    //    for (int i = 0; i < map.Length; i++)
-    //    {
-    //        if (map[i] == 1)
-    //        {
-    //            playerIndex = i;
-    //            break;
-    //        }
-    //    }
-    //    // playerの位置を返す 
-    //    return playerIndex;
-    //}
+    Vector2Int GetPlayerIndex()
+    {
 
-/////<summary>
-/////移動メソッド
-/////</summary>
-/////<param name="number">移動する数字</param>
-/////<prarm name="moveFrom">移動元の位置</param>
-/////<prarm name="moveTo">移動先の位置</param>
+        // playerの位置の検索
+        for (int y = 0; y < objectsMap.GetLength(0); y++)
+        {
+            for(int x = 0; x < objectsMap.GetLength(1); x++)
+            {
+                if (objectsMap[x, y] == null)
+                { continue; }
+                if (objectsMap[x, y].CompareTag("Player"))
+                {
+                    return new Vector2Int(x, y);
+                }
+            }
+        }
+        return new Vector2Int(-1, -1);
+    }
 
-
-//    bool MoveNumber(int number, int moveFrom, int moveTo)
-//    {
-//        //移動先が左端よりさき、まだは右端よりさきにいっているか?
-//        if (moveTo < 0 || moveTo >= map.Length)
-//        {
-//            return false;
-//        }
-//        // 移動先に箱がいたら
-//        if (map[moveTo] == 2)
-//        {
-//            //　移動方向的算出
-//            int velcity = moveTo - moveFrom;
-
-//            //playerの移動先に箱があるので、ｐぁいぇｒの移動先から箱をどかす
-//            // 箱はplayerの移動先になってしまっているので、playerの移動方向に合わせて
-//            //一つ動けるか確認する。動けたかどうかはsuccessに返却される
-//            bool sccess = MoveNumber(2,moveTo,moveTo + velcity);
-//            if (!sccess){ return false;}
-//        }
+    ///<summary>
+    ///移動メソッド
+    ///</summary>
+    ///<param name="number">移動する数字</param>
+    ///<prarm name="moveFrom">移動元の位置</param>
+    ///<prarm name="moveTo">移動先の位置</param>
 
 
-//            map[moveTo] = number;
-//            map[moveFrom] = 0;
+    bool MoveObject(Vector2 moveFrom, Vector2Int moveTo)
+    {
+        //移動先が左端よりさき、まだは右端よりさきにいっているか?
+        if (moveTo.y < 0 || moveTo.y >= map.GetLength(0))
+        {
+            return false;
+        }if (moveTo.x < 0 || moveTo.x >= map.GetLength(1))
+        {
+            return false;
+        }
 
-//        return true;
-//    }
+        objectsMap[(int)moveFrom.y, (int)moveFrom.x].transform.position =
+            new Vector3(moveTo.x,map.GetLength(0) - moveTo.y, 0);
+        objectsMap[moveTo.y,moveTo.x] = 
+            objectsMap[(int)moveFrom.y, (int)moveFrom.x]; 
+        
+        objectsMap[(int)moveFrom.y, (int)moveFrom.x] = null;
+        return true;
+
+       
+    }
 }
 
